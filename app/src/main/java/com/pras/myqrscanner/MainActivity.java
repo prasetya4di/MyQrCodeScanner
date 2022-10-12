5ddc386721a16ead2349d0b1b076075dbee30dcb
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2022-10-12 22:00:08 +0700

    Show and hide button when detect a qr code

--- app/src/main/java/com/pras/myqrscanner/MainActivity.java
+++ app/src/main/java/com/pras/myqrscanner/MainActivity.java
@@ -3,12 +3,16 @@ package com.pras.myqrscanner;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
@@ -26,12 +30,23 @@ public class MainActivity extends AppCompatActivity {
     private PreviewView cameraView;
     private ListenableFuture<ProcessCameraProvider> cameraProvider;
 
+    private Button qrCodeFoundButton;
+    private String qrCode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         cameraView = findViewById(R.id.mainCameraView);
+        qrCodeFoundButton = findViewById(R.id.qrCodeFoundButton);
+
+        qrCodeFoundButton.setVisibility(View.INVISIBLE);
+        qrCodeFoundButton.setOnClickListener(v -> {
+            Toast.makeText(getApplicationContext(), qrCode, Toast.LENGTH_SHORT).show();
+            Log.i(MainActivity.class.getSimpleName(), "QR Code Found: " + qrCode);
+        });
+
         cameraProvider = ProcessCameraProvider.getInstance(this);
         requestCamera();
     }
@@ -82,6 +97,24 @@ public class MainActivity extends AppCompatActivity {
 
         preview.setSurfaceProvider(cameraView.getSurfaceProvider());
 
-        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview);
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .setTargetResolution(new Size(1280, 720))
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build();
+
+        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), new QRCodeImageAnalyzer(new QRCodeFoundListener() {
+            @Override
+            public void onQRCodeFound(String _qrCode) {
+                qrCode = _qrCode;
+                qrCodeFoundButton.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void qrCodeNotFound() {
+                qrCodeFoundButton.setVisibility(View.INVISIBLE);
+            }
+        }));
+
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview);
     }
 }
