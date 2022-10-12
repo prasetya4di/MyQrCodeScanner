7b6a1c011293d602568da52e1ceb90e991a007a5
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2022-10-12 21:41:42 +0700

    Add camera view

--- app/src/main/java/com/pras/myqrscanner/MainActivity.java
+++ app/src/main/java/com/pras/myqrscanner/MainActivity.java
@@ -7,18 +7,32 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int PERMISSION_REQUEST_CAMERA = 0;
+    private PreviewView cameraView;
+    private ListenableFuture<ProcessCameraProvider> cameraProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        cameraView = findViewById(R.id.mainCameraView);
+        cameraProvider = ProcessCameraProvider.getInstance(this);
         requestCamera();
     }
 
@@ -47,6 +61,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startCamera() {
-        //todo
+        cameraProvider.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = this.cameraProvider.get();
+                bindCameraPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                Toast.makeText(this, "Error starting camera " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    private void bindCameraPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        cameraView.setImplementationMode(PreviewView.ImplementationMode.PERFORMANCE);
+
+        Preview preview = new Preview.Builder().build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        preview.setSurfaceProvider(cameraView.getSurfaceProvider());
+
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview);
     }
 }
